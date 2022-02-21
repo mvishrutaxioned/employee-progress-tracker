3be8cb5e35e19315acbd9f80e16911399574b130
VishrutMargam <vishrutm@axioned.com>
2022-02-21 12:11:25 +0530

    style: logShiftPage styling

---
+++ src/components/DayRadioBtn/DayRadioBtn.style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const DayRadioBtnStyle = styled.div`
+    p {
+        margin: 30px 0 5px 0;
+        font-weight: 600;
+        font-size: 14px;
+    }
+`;
