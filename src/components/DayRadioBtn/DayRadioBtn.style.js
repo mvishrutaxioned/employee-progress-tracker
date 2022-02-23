0decac1f151c7945006a268c5be4946e7dbbbbf2
VishrutMargam <vishrutm@axioned.com>
2022-02-23 19:01:05 +0530

    fix: deleted DayRadioBtn

--- src/components/DayRadioBtn/DayRadioBtn.style.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const DayRadioBtnStyle = styled.div`
-    p {
-        margin: 30px 0 5px 0;
-        font-weight: 600;
-        font-size: 14px;
-    }
-`;
